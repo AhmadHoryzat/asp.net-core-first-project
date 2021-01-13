fbf2adce2967acd12bc917bca16f8ac0fce8ec08
Ahmad Horyzat <171059@ppu.edu.ps>
2021-01-13 22:46:06 +0200

    read data from json file

---
+++ AspWebApplication1/Services/JsonFileProductService.cs
@@ -0,0 +1,38 @@
+﻿using AspWebApplication1.wwwroot.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AspWebApplication1.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment )
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+        public string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using(var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+                       
+            }
+        }
+    }
+}
